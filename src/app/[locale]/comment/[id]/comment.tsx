04febd5b739e09939e8f8de3d6fca95b6a093edd
Jayvee O. Cruz <jayveecruz05@gmail.com>
2024-01-10 18:27:13 +0800

    Code Optimization.

--- src/app/[locale]/comment/[id]/comment.tsx
+++ src/app/[locale]/comment/[id]/comment.tsx
@@ -29,7 +29,7 @@ const Comment = () => {
   const response: any = getComment(params.id)
   return (
     <>
-      <h1 className="title">{langTrans('comment/title')} {params.id}</h1>
+      <h1 className="title">{langTrans('comment/title', { id: params.id })}</h1>
       <Link className={styles['back-button']} href={defaultPath}>&#8678; {langTrans('button/back')}</Link>
       {(response?.isLoading) ? <p>{langTrans('data/loading')}</p> : (response?.isError && <p>{langTrans('data/error')}</p>) || (<div className={styles['properties-holder']}>{Values(langTrans, response?.data?.data)}</div>)}
     </>
