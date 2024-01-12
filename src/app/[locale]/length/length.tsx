da99dc075beb6e0d7b010a1d0549392522c00f96
Jayvee O. Cruz <jayveecruz05@gmail.com>
2024-01-12 22:04:31 +0800

    Code Optimization.

--- src/app/[locale]/length/length.tsx
+++ src/app/[locale]/length/length.tsx
@@ -25,58 +25,59 @@ const Length = () => {
   // Queries
   const response: any = getComments()
 
-  // const simpleCommentsList = useMemo(() => {
-  //   const sortedList = response?.data?.data?.concat()?.sort((a: any, b: any) => (String(a.body).length - String(b.body).length))
-  //   const averageLength = (response?.data?.data.reduce((previousCount: any, currentCount: any, currentIndex: any) => ((previousCount + String(currentCount.body).length) / ((currentIndex === (response?.data?.data?.length -1)) ? response?.data?.data?.length : 1)), 0))
-  //   const average = response?.data?.data?.reduce((previousData: any, currentData: any) => {
-  //     const itemLengthDifference = Math.abs(String(currentData?.body).length - averageLength);
-  //     const closestLengthDifference = Math.abs(String(previousData?.body).length - averageLength);
-  //     return ((itemLengthDifference < closestLengthDifference) ? { ...currentData, length: (String(currentData.body).length), type: 'Average', } : previousData)
-  //   }, response?.data?.data?.[0])
-  //   return {
-  //     shortest: { ...sortedList?.[0], type: 'Shortest', length: (String(sortedList?.[0]?.body).length) },
-  //     longest: { ...sortedList?.[sortedList.length - 1], type: 'Longest', length: (String(sortedList?.[sortedList.length - 1]?.body).length) },
-  //     average
-  //   }
-  // }, [response?.data?.data])
+  const simplifiedCommentsList = useMemo(() => {
+    const sortedList = response?.data?.data?.concat()?.sort((a: any, b: any) => (String(a.body).length - String(b.body).length))
+    const averageLength = (response?.data?.data.reduce((previousCount: any, currentCount: any, currentIndex: any) => ((previousCount + String(currentCount.body).length) / ((currentIndex === (response?.data?.data?.length -1)) ? response?.data?.data?.length : 1)), 0))
+    const average = response?.data?.data?.reduce((previousData: any, currentData: any) => {
+      const itemLengthDifference = Math.abs(String(currentData?.body).length - averageLength);
+      const closestLengthDifference = Math.abs(String(previousData?.body).length - averageLength);
+      return ((itemLengthDifference < closestLengthDifference) ? { ...currentData, length: (String(currentData.body).length), type: 'Average', } : previousData)
+    }, response?.data?.data?.[0])
+    return {
+      shortest: { ...sortedList?.[0], type: 'Shortest', length: (String(sortedList?.[0]?.body).length) },
+      longest: { ...sortedList?.[sortedList.length - 1], type: 'Longest', length: (String(sortedList?.[sortedList.length - 1]?.body).length) },
+      average
+    }
+  }, [response?.data?.data])
 
   // Get the shortest, longest and average length
-  const commentLength = useMemo(() => (response?.data?.data?.reduce((previousData: any, currentData: any, currentIndex: number) => {
-    const shortest = ((String(currentData.body).length < previousData?.shortest) ? (String(currentData.body).length) : previousData?.shortest)
-    const longest = ((String(currentData.body).length > previousData?.longest) ? (String(currentData.body).length) : previousData?.longest)
-    return {
-      shortest,
-      longest,
-      average: ((shortest + longest) / 2),
-      // average: Math.round((previousData?.average + currentData.body.length) / ((currentIndex === (response?.data?.data.length - 1)) ? response?.data?.data.length : 1))
-    }
-  }, { shortest: String(response?.data?.data?.[0]?.body).length, longest: String(response?.data?.data?.[0]?.body).length, average: 0 })), [response?.data?.data])
+  // const commentLength = useMemo(() => (response?.data?.data?.reduce((previousData: any, currentData: any, currentIndex: number) => {
+  //   const shortest = ((String(currentData.body).length < previousData?.shortest) ? (String(currentData.body).length) : previousData?.shortest)
+  //   const longest = ((String(currentData.body).length > previousData?.longest) ? (String(currentData.body).length) : previousData?.longest)
+  //   return {
+  //     shortest,
+  //     longest,
+  //     average: ((shortest + longest) / 2),
+  //     // average: Math.round((previousData?.average + currentData.body.length) / ((currentIndex === (response?.data?.data.length - 1)) ? response?.data?.data.length : 1))
+  //   }
+  // }, { shortest: String(response?.data?.data?.[0]?.body).length, longest: String(response?.data?.data?.[0]?.body).length, average: 0 })), [response?.data?.data])
 
   // Find the shortest, longest and average body length
-  const comment = useMemo(() => (response?.data?.data?.reduce((previousData: any, currentData: any) => {
-    const itemLengthDifference = Math.abs(String(currentData?.body).length - commentLength?.average);
-    const closestLengthDifference = Math.abs(String(previousData?.average?.body).length - commentLength?.average);
-    return {
-      shortest: ((String(currentData.body).length <= commentLength?.shortest) ? { ...currentData, length: (String(currentData.body).length), type: 'Shortest', } : previousData?.shortest),
-      longest: ((String(currentData.body).length >= commentLength?.longest) ? { ...currentData, length: (String(currentData.body).length), type: 'Longest', } : previousData?.longest),
-      average: ((itemLengthDifference < closestLengthDifference) ? { ...currentData, length: (String(currentData.body).length), type: 'Average', } : previousData?.average),
-      // averageList: [ ...previousData?.averageList, ...((itemLengthDifference <= 1) ? [ { ...currentData, length: (String(currentData.body).length), type: 'Average', } ] : []) ]
-    }
-  }, { shortest: response?.data?.data?.[0], longest: response?.data?.data?.[0], average: response?.data?.data?.[0], averageList: [] })), [response?.data?.data, commentLength]);
+  // const comment = useMemo(() => (response?.data?.data?.reduce((previousData: any, currentData: any) => {
+  //   const itemLengthDifference = Math.abs(String(currentData?.body).length - commentLength?.average);
+  //   const closestLengthDifference = Math.abs(String(previousData?.average?.body).length - commentLength?.average);
+  //   return {
+  //     shortest: ((String(currentData.body).length <= commentLength?.shortest) ? { ...currentData, length: (String(currentData.body).length), type: 'Shortest', } : previousData?.shortest),
+  //     longest: ((String(currentData.body).length >= commentLength?.longest) ? { ...currentData, length: (String(currentData.body).length), type: 'Longest', } : previousData?.longest),
+  //     average: ((itemLengthDifference < closestLengthDifference) ? { ...currentData, length: (String(currentData.body).length), type: 'Average', } : previousData?.average),
+  //     // averageList: [ ...previousData?.averageList, ...((itemLengthDifference <= 1) ? [ { ...currentData, length: (String(currentData.body).length), type: 'Average', } ] : []) ]
+  //   }
+  // }, { shortest: response?.data?.data?.[0], longest: response?.data?.data?.[0], average: response?.data?.data?.[0], averageList: [] })), [response?.data?.data, commentLength]);
   
   return (
     (response?.isLoading) ? <p>{langTrans('data/loading')}</p> :
     (response?.isError && <p>{langTrans('data/error')}</p>) ||
     <>
       <h3 className="title">{langTrans('title/graph/bar')}</h3>
-      <BarGraph data={[ { type: 'shortest', length: commentLength?.shortest }, { type: 'average', length: commentLength?.average }, { type: 'longest', length: commentLength?.longest } ]}/>
+      <BarGraph data={[ simplifiedCommentsList?.shortest, simplifiedCommentsList?.average, simplifiedCommentsList?.longest ]}/>
+      {/* <BarGraph data={[ { type: 'shortest', length: commentLength?.shortest }, { type: 'average', length: commentLength?.average }, { type: 'longest', length: commentLength?.longest } ]}/> */}
       <h3 className="title">{langTrans('title/comment/shortest')}</h3>
-      <div className={styles['properties-holder']}>{Values(langTrans, comment?.shortest)}</div>
+      <div className={styles['properties-holder']}>{Values(langTrans, simplifiedCommentsList?.shortest)}</div>
       <h3 className="title">{langTrans('title/comment/average')}</h3>
-      <div className={styles['properties-holder']}>{Values(langTrans, comment?.average)}</div>
+      <div className={styles['properties-holder']}>{Values(langTrans, simplifiedCommentsList?.average)}</div>
       {/* {comment?.averageList?.map((item: any) => (<div key={item?.id} className={styles['properties-holder']}>{Values(langTrans, item)}</div>))} */}
       <h3 className="title">{langTrans('title/comment/longest')}</h3>
-      <div className={styles['properties-holder']}>{Values(langTrans, comment?.longest)}</div>
+      <div className={styles['properties-holder']}>{Values(langTrans, simplifiedCommentsList?.longest)}</div>
     </>
   )
 }
